e4d08473cd6d8c474decc5e0449df2490af7edaf
Tharindu Ranaweera <39702122+InsanitySquared@users.noreply.github.com>
2020-04-05 11:44:52 +0530

    Update example-main.cpp

--- example/client-windows/example-main.cpp
+++ example/client-windows/example-main.cpp
@@ -4,7 +4,7 @@
 #include "serverCon.h"
 
 #ifndef debugOutput         //servercon.h internally includes iostream if debug output is enabled
-#include <iostream>
+    #include <iostream>
 #endif
 
 #define servPort "31079"
@@ -21,8 +21,47 @@ int main() {
     server.sendData("ct");
 
     server.readData(buffr);
-    if((buffr[0]!='a')||(buffr[2]!='f')||(buffr[4]!='m')){
+    if((buffr[0]!='a')||(buffr[1]!='f')||(buffr[4]!='m')){
         std::cout<<"hmmm... thats odd, wrong ack"<<std::endl;
+        return -1;
+    }
+
+    std::cout<<"waiting for other client device"<<std::endl;
+
+    bool proceed = false;
+    while (!proceed){
+            server.readData(buffr);
+        if (buffr[0]=='r'){
+            proceed=true;
+        }
+    }
+
+    std::cout<<"Second client device connected"<<std::endl;
+
+    bool quit=false;
+    while(!quit) {
+        char input;
+        std::cout << "What data do you want? (l -location h -current heading b -battery s -status g -gyro data q -to quit): ";
+        std::cin >> input;
+        switch (input) {
+            case 'l':
+                server.sendData("slx"); // request gps data from server- x axis
+                server.readData(buffr);
+                std::cout<<"X: "<<buffr<<std::endl;
+                server.sendData("sly"); // request gps data from server- y axis
+                server.readData(buffr);
+                std::cout<<"Y: "<<buffr<<std::endl;
+                break;
+
+            case 'q':
+                server.sendData("q");
+                quit=true;
+                break;
+                
+            default:
+                std::cout<<"Not supported yet"<<std::endl;
+                break;
+        }
     }
 
 
