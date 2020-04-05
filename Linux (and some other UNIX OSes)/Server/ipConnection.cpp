d17e431f3c3b19de25d4880e63c6c308609396c1
Tharindu Ranaweera <39702122+InsanitySquared@users.noreply.github.com>
2020-04-05 09:25:13 +0530

    Add files via upload

---
+++ Linux (and some other UNIX OSes)/Server/ipConnection.cpp
@@ -0,0 +1,109 @@
+//
+// Created by thari on 4/3/2020.
+//
+
+#include "ipConnection.h"
+
+int ipConnection::listenAndAcceptClient(int port) {
+
+    listenSock = socket(AF_INET, SOCK_STREAM, 0);     //create the listening socket and add FD to FD table
+    if (listenSock < 0){
+        #ifdef debugOutput
+        std::cout<<"Error creating Listening socket"<<std::endl;
+        #endif
+        return -1;
+    }
+    else{
+        #ifdef debugOutput
+        std::cout<<"Successfuly created listening socket!"<<std::endl;
+        #endif
+    }
+
+    bzero((char *) &serv_addr, sizeof(serv_addr));      //zero serv_addr
+
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(port);
+    serv_addr.sin_addr.s_addr = INADDR_ANY;
+
+    if (bind(listenSock, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0){       //bind listenenr to adrress
+        #ifdef debugOutput
+        std::cout<<"Listenner bind failed"<<std::endl;
+        #endif
+        return -2;
+    }
+    else{
+        #ifdef debugOutput
+        std::cout<<"Listenner successfully bound!"<<std::endl;
+        #endif
+    }
+
+    listen(listenSock, 5);          //listen for connections
+
+    clilen = sizeof(cli_addr);
+    connSock = accept(listenSock, (struct sockaddr *) &cli_addr, (socklen_t*)&clilen);
+    if (connSock<0){
+        #ifdef debugOutput
+        std::cout<<"Error on accepting client and creating new socket"<<std::endl;
+        #endif
+        return -3;
+    }
+    else{
+        #ifdef debugOutput
+        std::cout<<"Client connection successful!"<<std::endl;
+        #endif
+    }
+    close(listenSock);
+    return 1;
+}
+
+int ipConnection::sendData(char *buff, int len){
+
+    n = write(connSock,buff,len);
+    int y=0;
+    while ((n < 0) && (y<6)){
+        #ifdef debugOutput
+        std::cout<<"Error writing to client, trying again"<<std::endl;
+        #endif
+        n = write(connSock,buff,len);
+        y=y+1;
+    }
+    if(y>5) {
+        #ifdef debugOutput
+        std::cout << "Write failed" << std::endl;
+        #endif
+        return -1;
+    }
+    return 0;
+}
+
+int ipConnection::readData(char *data) {
+
+    bzero(data,bufflen);
+    n = read(connSock,data,(bufflen-1));
+    int y=0;
+    while ((n < 0) && (y<6)){
+        #ifdef debugOutput
+        std::cout<<"Error reading from client, trying again"<<std::endl;
+        #endif
+        n = read(connSock,data,511);
+        y=y+1;
+    }
+    if(y>5){
+        #ifdef debugOutput
+        std::cout<<"Read failed"<<std::endl;
+        #endif
+        return -1;
+    }
+    return 0;
+}
+
+int ipConnection::cleanUp() {
+    #ifdef debugOutput
+    std::cout << "Closing connections" << std::endl;
+    #endif
+    close(connSock);
+    return 0;
+}
+
+
+
---
+++ example/server-linux/ipConnection.h
@@ -0,0 +1,44 @@
+//
+// Created by thari on 4/3/2020.
+//
+
+#ifndef CTRL_VIA_IP_SERVER_IPCONNECTION_H
+#define CTRL_VIA_IP_SERVER_IPCONNECTION_H
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+#ifndef strings
+#include<strings.h>
+#endif
+#ifndef unistd
+#include<unistd.h>
+#endif
+
+#ifdef debugOutput
+#include <iostream>
+#endif
+
+#ifndef bufflen
+#define bufflen 512
+#endif
+
+class ipConnection {
+private:
+    int listenSock, connSock;     //init file descriptors for sockets
+    int clilen, n;        //init some variables
+
+    struct sockaddr_in serv_addr, cli_addr;
+
+public:
+    int listenAndAcceptClient(int port);
+    int cleanUp();
+    int sendData(char buff[bufflen], int len);
+    int readData(char data[bufflen]);
+
+
+};
+
+
+#endif //CTRL_VIA_IP_SERVER_IPCONNECTION_H
